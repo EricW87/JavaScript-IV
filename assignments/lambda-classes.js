3cc4b487c0dec3232864ec48b56e7a0a7b7a935b
Eric Wood <epwood1@gmail.com>
2019-09-12 18:16:55 -0700

    Finished MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,27 +2,27 @@
 
 class Person
 {
-	constructor(personAttributes)
+	constructor(attributes)
 	{
-		this.name = personAttributes.name;
-		this.age = personAttributes.age;
-		this.location = personAttributes.location;
+		this.name = attributes.name;
+		this.age = attributes.age;
+		this.location = attributes.location;
 	}
 
 	speak()
 	{
-		console.log(`Hello my name is ${this.name}, I am from ${this.Bedrock}`);
+		console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
 	}
 }
 
 class Instructor extends Person
 {
-	constructor(instructorAttributes)
+	constructor(attributes)
 	{
-		super(personAttributes);
-		this.specialty = instructorAttributes.specialty;
-		this.favLanguage = instructorAttributes.favLanguage;
-		this.catchPhrase = instrutorAttributes.catchPhrase;
+		super(attributes);
+		this.specialty = attributes.specialty;
+		this.favLanguage = attributes.favLanguage;
+		this.catchPhrase = attributes.catchPhrase;
 	}
 
 	demo(subject)
@@ -38,12 +38,12 @@ class Instructor extends Person
 
 class Student extends Person
 {
-	constructor(studentAttributes)
+	constructor(attributes)
 	{
-		super(personAttributes);
-		this.previousBackground = studentAttributes.previousBackground;
-		this.className = studentAttributes.className;
-		this.favSubjects = studentAttributes.favSubjects;
+		super(attributes);
+		this.previousBackground = attributes.previousBackground;
+		this.className = attributes.className;
+		this.favSubjects = attributes.favSubjects;
 	}
 
 	listsSubjects()
@@ -58,22 +58,22 @@ class Student extends Person
 
 	sprintChallenge(subject)
 	{
-		conse.log(`${this.name} has begun sprint challenge on ${subject}`);
+		console.log(`${this.name} has begun sprint challenge on ${subject}`);
 	}
 }
 
 class ProjectManager extends Instructor
 {
-	constructor(managerAttributes)
+	constructor(attributes)
 	{
-		super(instructorAttributes);
-		this.gradClassName = managerAttributes.gradClassName;
-		this.favInstructor = managerAttributes.favInstructor;
+		super(attributes);
+		this.gradClassName = attributes.gradClassName;
+		this.favInstructor = attributes.favInstructor;
 	}
 
 	standUp(channel)
 	{
-		console.log(`${this.name} announces to ${this.channel}, @${this.channel} standy times!`);
+		console.log(`${this.name} announces to ${channel}, @${channel} standy times!`);
 	}
 
 	debugsCode(student, subject)
@@ -81,3 +81,54 @@ class ProjectManager extends Instructor
 		console.log(`${this.name} debugs ${student.name}\'s code on ${subject}`);
 	}
 }
+
+const joe = new Person
+({
+	name: 'Joe',
+	age: '32',
+	location: 'Oceania'
+});
+
+const fred = new Instructor
+({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+const wilma = new Student
+({
+	name: 'Wilma',
+	location: 'Bedrock',
+	age: 33,
+	previousBackground: 'Dino hunting',
+	className: 'WEBPT10',
+	favSubjects: [ 'Javascript', 'HTML', 'flex-box' ]
+});
+
+const michael = new ProjectManager
+({
+	name: 'Michael',
+	location: 'New York',
+	age: 25,
+	favLanguage: 'JavaScript',
+	specialty: 'Back-end',
+	catchPhrase: 'Lambda school',
+	gradClassName: 'WEBPT10',
+	favInstructor: 'Keiran'
+});
+
+console.log(joe);
+joe.speak();
+console.log(fred);
+fred.demo('classes');
+console.log(wilma);
+wilma.listsSubjects();
+wilma.PRAssignment('Web Apps');
+wilma.sprintChallenge('Javascript');
+console.log(michael);
+michael.standUp('webpt10_michael');
+michael.debugsCode(wilma, 'CSS');
